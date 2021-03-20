5ce28a06737f2148cc13da6aa99fcad43fba8809
ArkadiuszPlaza <79998737+ArkadiuszPlaza@users.noreply.github.com>
2021-03-20 14:46:14 +0100

    Add files via upload

--- inc/LZespolona.hh
+++ inc/LZespolona.hh
@@ -1,6 +1,9 @@
 #ifndef LZESPOLONA_HH
 #define LZESPOLONA_HH
+#include <iostream>
+#include <cmath>
 
+using namespace std;
 /*!
  *  Plik zawiera definicje struktury LZesplona oraz zapowiedzi
  *  przeciazen operatorow arytmetycznych dzialajacych na tej 
@@ -16,15 +19,19 @@ struct  LZespolona {
   double   im;    /*! Pole repezentuje czesc urojona. */
 };
 
+void Wyswietl(LZespolona arg);
+void Wczytaj(LZespolona &arg);
 
 /*
  * Dalej powinny pojawic sie zapowiedzi definicji przeciazen operatorow
  */
 
-bool  operator == (LZespolona  Skl1,  LZespolona  Skl2);
-
 LZespolona  operator + (LZespolona  Skl1,  LZespolona  Skl2);
-
+LZespolona operator - (LZespolona Skl1, LZespolona Skl2);
+LZespolona operator * (LZespolona Skl1, LZespolona Skl2);
+LZespolona Sprzezenie(LZespolona arg);
+double Modul2(LZespolona arg);
 LZespolona operator / (LZespolona Skl1, double Skl2);
+LZespolona operator / (LZespolona Skl1, LZespolona Skl2);
 
 #endif
